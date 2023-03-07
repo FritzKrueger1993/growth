61d53be142cb80658a6f61a96586c1681113f19e
FritzKrueger1993 <109686983+FritzKrueger1993@users.noreply.github.com>
2023-03-07 23:36:15 +0100

    Update Gravity_Grow.js

--- Gravity_Grow.js
+++ Gravity_Grow.js
@@ -12,68 +12,68 @@ let displayComp = 5;
 let count = 50;
 let growing = 0.00005;
 let textColor = 255;
-let hypo = []; 
+let hypo = [];
 
 function setup() {
   createCanvas (windowWidth - displayComp, windowHeight - displayComp);
   background(0);
   setPos();
-} 
+}
 
 
 function draw() {
- background(0);
- fill(255,255,255,255);
- stroke(0,0,0,50);
- for (i = 0; i<= count; i++) {
-  difX[i] = abs(mainX - x[i]);
-  difY[i] = abs(mainY - y[i]);
-  gravityX[i] = map(difX[i], 0, width, 0.003, 0.000000001);
-  gravityY[i] = map(difY[i], 0, height, 0.003, 0.000000001);
-  hypo[i] = sqrt( pow(difX[i],2) + pow(difY[i],2) );
-  if( hypo[i] <= mainDiameter ) {
-    gravityX[i] *= 1.5;
-    gravityY[i] *= 1.5;
+  background(0);
+  fill(255, 255, 255, 255);
+  stroke(0, 0, 0, 50);
+  for (i = 0; i<= count; i++) {
+    difX[i] = abs(mainX - x[i]);
+    difY[i] = abs(mainY - y[i]);
+    gravityX[i] = map(difX[i], 0, width, 0.003, 0.000000001);
+    gravityY[i] = map(difY[i], 0, height, 0.003, 0.000000001);
+    hypo[i] = sqrt( pow(difX[i], 2) + pow(difY[i], 2) );
+    if ( hypo[i] <= mainDiameter ) {
+      gravityX[i] *= 1.5;
+      gravityY[i] *= 1.5;
+    }
+    x[i] = lerp(x[i], mainX, gravityX[i]);
+    y[i] = lerp(y[i], mainY, gravityY[i]);
+    if (difX[i] <= mainDiameter * 0.3 && difY[i] <= mainDiameter * 0.3) {
+      x[i] = random(0, width);
+      y[i] = random(0, height);
+    }
+    circle(x[i], y[i], circleDiameter[i]);
   }
-  x[i] = lerp(x[i], mainX, gravityX[i]);
-  y[i] = lerp(y[i], mainY, gravityY[i]);
-  if(difX[i] <= mainDiameter * 0.3 && difY[i] <= mainDiameter * 0.3){
-  x[i] = random(0, width);
-  y[i] = random(0, height);
+  growing *= 1.003;
+  mainDiameter = lerp(mainDiameter, width*4, growing);
+  circle(mainX, mainY, mainDiameter);
+  if (mainDiameter >= width*1.7 && mainDiameter >= height*1.7) {
+    textColor=lerp(textColor, 0, 0.01 );
+    if (windowWidth <= 500) {
+      textSize(40);
+    } else {
+      textSize(100);
+    }
+    fill(textColor);
+    textSize(100);
+    textAlign(CENTER, CENTER);
+    text('Is there a limit to grwoth?', width/2, height/2);
   }
-  circle(x[i],y[i],circleDiameter[i]);
- }
- growing *= 1.003;
- mainDiameter = lerp(mainDiameter, width*4, growing);
- circle(mainX, mainY, mainDiameter); 
- if (mainDiameter >= width*1.7 && mainDiameter >= height*1.7) {
-  textColor=lerp(textColor, 0, 0.01 );
-  fill(textColor);
-  textSize(100);
-  textAlign(CENTER, CENTER);
-  text('Is there a limit to grwoth?', width/2, height/2);
 }
-}
-
-
-
-
-
 
 
 function windowResized() {
   resizeCanvas(windowWidth - displayComp, windowHeight - displayComp);
   setPos();
 }
-  
-  
-function setPos(){
 
- for (i = 0; i<= count; i++) {
-  x[i] = random(0, width);
-  y[i] = random(0, height);
-  circleDiameter[i] = random(10,40);
- }
+
+function setPos() {
+
+  for (i = 0; i<= count; i++) {
+    x[i] = random(0, width);
+    y[i] = random(0, height);
+    circleDiameter[i] = random(10, 40);
+  }
   mainX = random(0, width);
   mainY = random(0, height);
   mainDiameter = 60;
@@ -81,7 +81,7 @@ function setPos(){
   textColor = 255;
 }
 
-function mousePressed(){
+function mousePressed() {
   setPos();
   fullscreen(1);
 }
