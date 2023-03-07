e4dd703f568d7fbe6444b3e39d89a1dfcf367e1a
FritzKrueger1993 <109686983+FritzKrueger1993@users.noreply.github.com>
2023-03-07 23:27:03 +0100

    Update Gravity_Grow.js

--- Gravity_Grow.js
+++ Gravity_Grow.js
@@ -9,8 +9,8 @@ let mainX;
 let mainY;
 let mainDiameter;
 let displayComp = 5;
-let count = 400;
-let growing = 0.0001;
+let count = 50;
+let growing = 0.00005;
 let textColor = 255;
 let hypo = []; 
 
@@ -32,8 +32,8 @@ function draw() {
   gravityY[i] = map(difY[i], 0, height, 0.003, 0.000000001);
   hypo[i] = sqrt( pow(difX[i],2) + pow(difY[i],2) );
   if( hypo[i] <= mainDiameter ) {
-    gravityX[i] *= 2;
-    gravityY[i] *= 2;
+    gravityX[i] *= 1.5;
+    gravityY[i] *= 1.5;
   }
   x[i] = lerp(x[i], mainX, gravityX[i]);
   y[i] = lerp(y[i], mainY, gravityY[i]);
@@ -43,10 +43,10 @@ function draw() {
   }
   circle(x[i],y[i],circleDiameter[i]);
  }
- growing *= 1.005;
- mainDiameter = lerp(mainDiameter, width*2.5, growing);
+ growing *= 1.003;
+ mainDiameter = lerp(mainDiameter, width*4, growing);
  circle(mainX, mainY, mainDiameter); 
- if (mainDiameter >= width*2 && mainDiameter >= height*2) {
+ if (mainDiameter >= width*1.7 && mainDiameter >= height*1.7) {
   textColor=lerp(textColor, 0, 0.01 );
   fill(textColor);
   textSize(100);
@@ -68,6 +68,7 @@ function windowResized() {
   
   
 function setPos(){
+
  for (i = 0; i<= count; i++) {
   x[i] = random(0, width);
   y[i] = random(0, height);
@@ -82,4 +83,5 @@ function setPos(){
 
 function mousePressed(){
   setPos();
+  fullscreen(1);
 }
